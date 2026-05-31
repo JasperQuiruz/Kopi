abd185949ad370ca7779599d78f995cf2de8d023
JasperQuiruz <105185983+JasperQuiruz@users.noreply.github.com>
2026-05-31 13:52:47 +0800

    Update service worker assets and remove cache name
    Removed CACHE_NAME and added new asset files.

--- sw.js
+++ sw.js
@@ -1,24 +1,8 @@
-const CACHE_NAME = 'coin-calc-v1';
 const ASSETS = [
   'index.html',
   'manifest.json',
-  'HD-wallpaper-lucky-clover-beautiful-green-life-love-luck-nature.jpg'
+  'HD-wallpaper-lucky-clover-beautiful-green-life-love-luck-nature.jpg',
+  'settings.png',
+  'peso.png',
+  'dollar.png'
 ];
-
-// Install Service Worker and Cache Assets
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
-});
-
-// Cache and Network Strategy
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((cachedResponse) => {
-      return cachedResponse || fetch(e.request);
-    })
-  );
-});
